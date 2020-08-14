cb0b027db1e98dcb27f90318db9bd1027bc24bec
qili.taoqili <qili.taoqili@alibaba-inc.com>
2020-08-14 11:31:51 +0800

    v1.5.4

--- packages/template-react-deep/template/component/_eslintrc.js
+++ packages/template-react-deep/template/component/_eslintrc.js
@@ -1,5 +1,12 @@
 const { tslint, deepmerge } = require('@ice/spec');
 
 module.exports = deepmerge(tslint, {
+  env: {
+    jest: true,
+  },
+  globals: {
+    Promise: 'readonly',
+    React: 'readonly'
+  },
   rules: {},
 });
