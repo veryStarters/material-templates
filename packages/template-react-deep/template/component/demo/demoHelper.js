4b4a2ebf7c0e359cdab4d233716e51cbe9f5dd5a
qili.taoqili <qili.taoqili@alibaba-inc.com>
2020-08-14 11:42:45 +0800

    v1.5.2

--- packages/template-react-deep/template/component/demo/demoHelper.js
+++ packages/template-react-deep/template/component/demo/demoHelper.js
@@ -1,41 +1,49 @@
-const fixDevice = (instance) => {
-  const ua = navigator.userAgent
+const fixDevice = () => {
+  const ua = navigator.userAgent;
   const android = ua.match(/(Android);?[\s\/]+([\d.]+)?/);
   const ipad = ua.match(/(iPad).*OS\s([\d_]+)/);
   const iphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/);
   if (!android && !iphone && !ipad) {
-    return
+    return;
   }
-  const head = document.getElementsByTagName('head')[0]
-  const meta = document.createElement('meta')
-  const container = document.querySelector('.container')
-  const myContainer = document.querySelector('.my-container')
-  const customStyle = document.querySelector('.preview style')
-  meta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0,shrink-to-fit=no,user-scalable=no'
+  const head = document.getElementsByTagName('head')[0];
+  const meta = document.createElement('meta');
+  const container = document.querySelector('.container');
+  const myContainer = document.querySelector('.my-container');
+  const customStyle = document.querySelector('.preview style');
+  meta.content = 'width=device-width,minimum-scale=1.0,maximum-scale=1.0,shrink-to-fit=no,user-scalable=no';
   meta.name = 'viewport';
   head.appendChild(meta);
   if (customStyle) {
-    head.appendChild(customStyle)
+    head.appendChild(customStyle);
   }
-  document.body.appendChild(myContainer)
-  container.parentNode.removeChild(container)
-}
+  document.body.appendChild(myContainer);
+  container.parentNode.removeChild(container);
+};
 
 const handleResize = (instance) => {
   const width = document.documentElement.offsetWidth;
   const { device } = instance.state;
-  let realDevice = width < 480 ? 'phone' : width > 1024 ? 'desktop' : 'tablet';
+  let realDevice = 'desktop';
+  if (width < 480) {
+    realDevice = 'phone';
+  }
+  if (width >= 480 && width < 1024) {
+    realDevice = 'tablet';
+  }
   if (realDevice !== device) {
     instance.setState({
-      device: realDevice
+      device: realDevice,
     });
   }
-}
+};
 
 export default {
   init:(instance) => {
     handleResize(instance);
-    fixDevice(instance);
-    window.addEventListener('resize', () => { handleResize(instance) }, false)
+    fixDevice();
+    window.addEventListener('resize', () => {
+      handleResize(instance)
+    }, false);
   }
 }
